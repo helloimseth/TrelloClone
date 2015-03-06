236643f5c95a407cfba6a9777c6a4809f1e1aa53
Seth Hamlin <helloimseth@gmail.com>
2015-03-06 11:49:04 -0500

    Create new view template, view, working on implementing dropdown rendering

---
+++ app/assets/javascripts/views/newBoardView.js
@@ -0,0 +1,31 @@
+TrelloClone.Views.newBoardView = Backbone.View.extend({
+  template: JST['new_board_view'],
+
+  tagName: 'form',
+
+  className: 'new-board-form',
+
+  events: {
+    'submit': 'submitNewBoard'
+  },
+
+  render: function () {
+    var formTemplate = this.template();
+
+    this.$el.html(formTemplate);
+
+    return this
+  },
+
+  submitNewBoard: function (event) {
+    event.preventDefault;
+
+    var attrs = this.$el.serializeJSON();
+
+    var board = TrelloClone.Collections.boards.create(attrs);
+
+    Backbone.history.navigate("", {
+      trigger: false
+    })
+  }
+});
