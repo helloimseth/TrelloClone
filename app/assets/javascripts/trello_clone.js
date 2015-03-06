909ce093ca201aeb3e8ec292f928d7d2f8a1eedc
Michael Doliner <msd83@cornell.edu>
2015-03-06 10:38:46 -0500

    Initial commit

---
+++ app/assets/javascripts/trello_clone.js
@@ -0,0 +1,12 @@
+window.TrelloClone = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+  }
+};
+
+$(document).ready(function(){
+  TrelloClone.initialize();
+});
